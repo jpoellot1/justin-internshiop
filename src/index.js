9c2337627921b5a5105970be6a65ae025d54b489
jpoellot1 <j.poellot1@gmail.com>
2026-07-24 16:45:08 -0500

    Hot collections section completed

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom'; 
+import $ from 'jquery'
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+window.jquery = $;
+window.$ = $;
 
 ReactDOM.render(
   <React.StrictMode>
